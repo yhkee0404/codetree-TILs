9e071019215de54d901d6720149314c5a0b97926
yhkee0404 <yhkee0404@gmail.com>
2024-02-11 00:15:07 +0900

    Title: 트리 판별, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240211/트리 판별/tree-identification.py
+++ 240211/트리 판별/tree-identification.py
@@ -1,71 +1,44 @@
-MAX_N = 10000
+with open(0) as f:
+    m, *nodes = map(int, f.read().split())
 
-# 변수 선언 및 입력:
-m = int(input())
-root = 0
-deg = [0] * (MAX_N + 1)
-edges = [[] for _ in range(MAX_N + 1)]
-used = [False] * (MAX_N + 1)
-visited = [False] * (MAX_N + 1)
-is_tree = True
-
-# n개의 간선 정보를 입력받습니다.
-for _ in range(m):
-    x, y = tuple(map(int, input().split()))
-
-    # 간선 정보를 인접리스트에 넣어줍니다.
-    edges[x].append(y)
-
-    # 해당 번호가 그래프에 있는 정점 번호인지 판단합니다.
-    used[x] = True
-    used[y] = True
-
-    # 정점 별 들어오는 간선의 개수를 저장합니다.
-    deg[y] += 1
-
-
-# DFS를 통해 루트로부터 갈 수 있는 모든 정점을 탐색합니다.
-def dfs(x):
-    for y in edges[x]:
-        # 이미 방문한 노드는 스킵합니다.
-        if visited[y]: 
+def solve(m, nodes):
+    
+    ss = nodes[::2]
+    es = nodes[1::2] 
+    max_s = max(s for s in ss)
+    max_e = max(e for e in es)
+    
+    visited = [False] * (max(max_s, max_e) + 1)
+    
+    for e in es:
+        visited[e] = True
+    root = None
+    for s in ss:
+        if visited[s]:
             continue
-
-        visited[y] = True  
-        dfs(y)
-
-    return
-
-
-# 루트 노드를 찾습니다. 들어오는 간선이 하나도 없는 노드가 여러개면 트리가 아닙니다.
-for i in range(1, MAX_N + 1):
-    if used[i] and deg[i] == 0:
-        # 이미 선정된 루트가 있다면 
-        # 루트가 여러 개인 것이므로 트리가 아닙니다.
-        if root != 0: 
-            is_tree = False
-        root = i
-
-# 루트 노드가 없으면 트리가 아닙니다.
-if root == 0: 
-    is_tree = False
-
-# 루트 노드를 제외한 노드는 모두 들어오는 간선이 1개씩 있습니다. 그렇지 않으면 트리가 아닙니다.
-# for i in range(1, MAX_N + 1):
-#     if used[i] and i != root and deg[i] != 1:
-#         is_tree = False
-
-if is_tree and root != 0:
-    # root 정점으로부터 모든 정점을 갈 수 있는지 판단합니다.
+        if root is None:
+            root = s
+            continue
+        if root != s:
+            return False
+    if root is None:
+        return False
+    
+    adj = [[] for _ in visited]
+    for i in range(0, m << 1, 2):
+        adj[nodes[i]].append(nodes[i | 1])
+    
+    stack = [root]
+    for i in range(len(visited)):
+        visited[i] = False
     visited[root] = True
-    dfs(root)
+    while stack:
+        u = stack.pop()
+        for v in adj[u]:
+            if visited[v]:
+                return False
+            visited[v] = True
+            stack.append(v)
+    return all(visited[e] for e in es)
 
-# root 정점으로부터 탐색해 도달하지 못하는 정점이 있으면 트리가 아닙니다.
-for i in range(1, MAX_N + 1):
-    if used[i] and not visited[i]:
-        is_tree = False
-
-if is_tree: 
-    print(1)
-else:
-    print(0)
+print(1 if solve(m, nodes) else 0)
