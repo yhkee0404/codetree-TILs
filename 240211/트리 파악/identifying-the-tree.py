6eba0404cd5d26bd3d2612a629d7867ca2e79696
yhkee0404 <yhkee0404@gmail.com>
2024-02-11 22:14:55 +0900

    Title: 트리 파악, Time: 909ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240211/트리 파악/identifying-the-tree.py
+++ 240211/트리 파악/identifying-the-tree.py
@@ -10,12 +10,12 @@ with open(0) as f:
 
 sys.setrecursionlimit(n + 10)
 
-def solve(adj, u, v, depth = 0):
+def solve(u, v, depth = 0):
     depth += 1
     ans = 0
     for w in adj[v]:
         if w != u:
-            ans += solve(adj, v, w, depth)
+            ans += solve(v, w, depth)
     return ans if ans else depth - 1
 
-print(solve(adj, -1, 1) & 1)
+print(solve(-1, 1) & 1)
