88907c3a3bbdc5a1501992b6247e09bf5b3e44cd
yhkee0404 <yhkee0404@gmail.com>
2024-02-11 21:43:29 +0900

    Title: 트리 파악, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240211/트리 파악/identifying-the-tree.py
@@ -0,0 +1,19 @@
+with open(0) as f:
+    n = int(f.readline().strip())
+    adj = [[] for _ in range(n + 1)]
+    for _ in range(n - 1):
+        a, b = map(int, f.readline().split())
+        adj[a].append(b)
+        adj[b].append(a)
+
+def solve(adj, u, v, depth = 0):
+    if len(adj[v]) == 1:
+        return depth
+    depth += 1
+    return sum(
+        solve(adj, v, w, depth)
+        for w in adj[v]
+        if w != u
+    )
+
+print(solve(adj, -1, 1) ^ 1)
