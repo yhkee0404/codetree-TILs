b4c8fc169c5d2c9acbd1a6fee80354ade0405123
yhkee0404 <yhkee0404@gmail.com>
2024-02-11 22:09:20 +0900

    Title: 트리 파악, Time: 1308ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240211/트리 파악/identifying-the-tree.py
+++ 240211/트리 파악/identifying-the-tree.py
@@ -1,3 +1,5 @@
+import sys
+
 with open(0) as f:
     n = int(f.readline().strip())
     adj = [[] for _ in range(n + 1)]
@@ -6,19 +8,15 @@ with open(0) as f:
         adj[a].append(b)
         adj[b].append(a)
 
-def solve(adj):
-    ans = 0
-    stack = [(-1, 1, 0)]
-    while stack:
-        u, v, depth = stack.pop()
-        depth += 1
-        pushed = False
-        for w in adj[v]:
-            if w != u:
-                stack.append((v, w, depth))
-                pushed = True
-        if not pushed:
-            ans += depth - 1
-    return ans
+sys.setrecursionlimit(n + 10)
 
-print(solve(adj) & 1)
+def solve(adj, u, v, depth = 0):
+    depth += 1
+    ans = sum(
+        solve(adj, v, w, depth)
+        for w in adj[v]
+        if w != u
+    )
+    return ans if ans else depth - 1
+
+print(solve(adj, -1, 1) & 1)
