f35d08e4fe6cd3c961cc58e19847fa9e28cd4f53
yhkee0404 <yhkee0404@gmail.com>
2024-02-19 21:17:43 +0900

    Title: 노드의 공통 조상 2, Time: 5084ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

---
+++ 240219/노드의 공통 조상 2/common-ancestor-of-node-2.py
@@ -0,0 +1,65 @@
+def init_parents(n, adj, parents, depths, src):
+    du = depths[src] + 1
+    for u in adj[src]:
+        if depths[u] is None:
+            depths[u] = du
+            parents[0][u] = src
+            init_parents(n, adj, parents, depths, u)
+
+def make_parents(n, adj):
+    parents = [[None] * (n + 1) for _ in range(16)]
+    depths = [None] * (n + 1)
+    depths[1] = 0
+    init_parents(n, adj, parents, depths, 1)
+    di = 1
+    for i in range(1, 16):
+        pu, pv = parents[i - 1: i + 1]
+        for j in range(2, n + 1):
+            if depths[j] >= di:
+                pv[j] = pu[pu[j]]
+        di <<= 1
+    return parents, depths
+
+def lca(parents, depths, a, b):
+    da = depths[a]
+    db = depths[b]
+    diff = da - db
+    if diff < 0:
+        a, b, da, db = b, a, db, da
+        diff = - diff
+    i = 1
+    j = 0
+    while diff:
+        if diff & 1:
+            diff -= i
+            a = parents[j][a]
+        i <<= 1
+        j += 1
+    if a == b:
+        return a
+    i = 1 << 15
+    for pj in range(15, -1, -1):
+        if i > db:
+            continue
+        pj = parents[j]
+        if pj[a] == pj[b]:
+            continue
+        a = pj[a]
+        b = pj[b]
+    return parents[0][a]
+
+with open(0) as f:
+    n = int(f.readline().strip())
+    adj = [[] for _ in range(n + 1)]
+    for _ in range(n - 1):
+        a, b = map(int, f.readline().split())
+        adj[a].append(b)
+        adj[b].append(a)
+    parents, depths = make_parents(n, adj)
+    q = int(f.readline().strip())
+    ans = []
+    for _ in range(q):
+        a, b = map(int, f.readline().split())
+        ans.append(lca(parents, depths, a, b))
+
+print('\n'.join(map(str, ans)))
