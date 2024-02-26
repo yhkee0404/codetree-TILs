96b7aaf0ff407ebd595218765262bfa2f6bb870b
yhkee0404 <yhkee0404@gmail.com>
2024-02-26 13:19:12 +0900

    Title: 집합의 원소, Time: 377ms, Memory: 31MB, Status: Passed - CodeTree

--- 240226/집합의 원소/elements-of-a-set.py
+++ 240226/집합의 원소/elements-of-a-set.py
@@ -6,19 +6,26 @@ def find(parent, a):
     parent[a] = find(parent, parent[a])
     return parent[a]
 
-def union(parent, a, b):
+def union(parent, rank, a, b):
     a = find(parent, a)
     b = find(parent, b)
-    if a != b:
-        parent[a] = b
+    if a == b:
+        return
+    diff = rank[a] - rank[b]
+    if diff < 0:
+        a, b = b, a
+    elif diff == 0:
+        rank[a] += 1
+    parent[b] = a
 
 with open(0) as f:
     n, m = map(int, f.readline().split())
     parent = list(range(n + 1))
+    rank = [0] * (n + 1)
     for _ in range(m):
         a, b, c = map(int, f.readline().split())
         if a == 0:
-            union(parent, b, c)
+            union(parent, rank, b, c)
         else:
             ans.append(
                 '1' if find(parent, b) == find(parent, c)
