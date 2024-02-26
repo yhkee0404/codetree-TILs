ec865fa3bdbbb72c8c50896aa52ffd175052dd3d
yhkee0404 <yhkee0404@gmail.com>
2024-02-26 21:04:08 +0900

    Title: 최소 스패닝 트리 8, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240226/최소 스패닝 트리 8/minimum-spanning-tree-8.py
+++ 240226/최소 스패닝 트리 8/minimum-spanning-tree-8.py
@@ -1,22 +1,22 @@
 with open(0) as f:
     n, m = map(int, f.readline().split())
-    adj = [[500_000] * (n + 1) for _ in range(n + 1)]
+    adj = [[5_000_000] * (n + 1) for _ in range(n + 1)]
     for _ in range(m):
         a, b, c = map(int, f.readline().split())
         adj[a][b] = c
         adj[b][a] = c
 
 def findSource(n, dist):
-    dj = 500_000
+    dj = 5_000_000
     j = 0
     for i in range(1, n + 1):
         di = dist[i]
-        if 0 <= di < dj:
+        if 1 <= di < dj:
             dj = di
             j = i
     return j
 
-dist = [500_000] * (n + 1)
+dist = [5_000_000] * (n + 1)
 dist[1] = 1
 while True:
     src = findSource(n, dist)
@@ -27,4 +27,4 @@ while True:
     for i in range(1, n + 1):
         dist[i] = min(dist[i], ai[i])
 
-print(- sum(dist) + 500_000 - 1)
+print(- sum(dist) + 5_000_000 - 1)
