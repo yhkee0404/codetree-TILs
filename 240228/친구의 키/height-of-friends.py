b258ca3d8bbd8e767b59a3b3f30a060a2d99ae81
yhkee0404 <yhkee0404@gmail.com>
2024-02-28 00:56:45 +0900

    Title: 친구의 키, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240228/친구의 키/height-of-friends.py
@@ -0,0 +1,21 @@
+with open(0) as f:
+    n, m = map(int, f.readline().split())
+    adj = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        a, b = map(int, f.readline().split())
+        adj[a].append(b)
+
+visited = [False] * (n + 1)
+def dfs(adj, visited, order, src):
+    if visited[src]:
+        return
+    visited[src] = True
+    for dest in adj[src]:
+        dfs(adj, visited, order, dest)
+    order.append(src)
+
+order = []
+for i in range(1, n + 1):
+    dfs(adj, visited, order, i)
+
+print(' '.join(map(str, order[::-1])))
