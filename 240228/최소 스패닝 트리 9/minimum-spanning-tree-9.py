b57b67d215e0d5a55d9e2313178417bdc84f5e36
yhkee0404 <yhkee0404@gmail.com>
2024-02-28 23:26:06 +0900

    Title: 최소 스패닝 트리 9, Time: 630ms, Memory: 40MB, Status: Passed - CodeTree

---
+++ 240228/최소 스패닝 트리 9/minimum-spanning-tree-9.py
@@ -0,0 +1,24 @@
+from heapq import heappush, heappop
+
+with open(0) as f:
+    n, m = map(int, f.readline().split())
+    adj = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        a, b, c = map(int, f.readline().split())
+        adj[a].append((b, c))
+        adj[b].append((a, c))
+
+dist = [1_000_000_000] * (n + 1)
+dist[1] = 0
+pq = [(0, 1)]
+while pq:
+    du, u = heappop(pq)
+    if du != dist[u]:
+        continue
+    dist[u] = - dist[u]
+    for v, w in adj[u]:
+        if w < dist[v]:
+            dist[v] = w
+            heappush(pq, (w, v))
+
+print(- sum(dist) + 1_000_000_000)
