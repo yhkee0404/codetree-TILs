262efeb07aab84f1518026985dfe4b780cc2d470
yhkee0404 <yhkee0404@gmail.com>
2024-03-01 23:53:20 +0900

    Title: bit 계산, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240301/bit 계산/bit-calculation.py
@@ -0,0 +1,20 @@
+ans = []
+
+bit = 0
+with open(0) as f:
+    q = int(f.readline().strip())
+    for _ in range(q):
+        cmd, *args = f.readline().strip().split()
+        match cmd[0]:
+            case 'a':
+                bit |= 1 << int(args[0])
+            case 'd':
+                bit &= ~ 1 << int(args[0])
+            case 'p':
+                ans.append('1' if bit & 1 << int(args[0]) else '0')
+            case 't':
+                bit ^= 1 << int(args[0])
+            case 'c':
+                bit = 0
+
+print('\n'.join(ans))
