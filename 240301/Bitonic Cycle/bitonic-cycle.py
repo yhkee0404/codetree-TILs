8649fa3c0665278f986780aa2f9d3a6cfa086611
yhkee0404 <yhkee0404@gmail.com>
2024-03-01 22:38:42 +0900

    Title: Bitonic Cycle, Time: 554ms, Memory: 43MB, Status: Passed - CodeTree

--- 240301/Bitonic Cycle/bitonic-cycle.py
+++ 240301/Bitonic Cycle/bitonic-cycle.py
@@ -2,14 +2,15 @@ with open(0) as f:
     n = int(f.readline().strip())
     points = sorted(tuple(map(int, line.split())) for line in f.readlines())
 
-d = lambda points: sum(map(lambda x: x * x, (points[0][i] - points[1][i] for i in range(len(points[0])))))
+def d(a, b):
+    return sum(map(lambda x: x * x, (a[i] - b[i] for i in range(len(a)))))
 
 adj = [[None] * n for _ in range(n)]
 for i in range(n):
     ai = adj[i]
     pi = points[i]
     for j in range(i + 1, n):
-        ai[j] = d((pi, points[j]))
+        ai[j] = d(pi, points[j])
 
 MAX_DP = 1_000_000_000_000_000_000
 dp = [[MAX_DP] * n for _ in range(n)]
