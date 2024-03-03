2c365a504ab2af2337a11e0c0de1df3e2b03f145
yhkee0404 <yhkee0404@gmail.com>
2024-03-03 19:30:14 +0900

    Title: 외판원 순회 2, Time: 545ms, Memory: 37MB, Status: Passed - CodeTree

---
+++ 240303/외판원 순회 2/traveling-salesman-problem-2.py
@@ -0,0 +1,21 @@
+with open(0) as f:
+    n = int(f.readline().strip())
+    a = tuple(tuple(map(int, line.split())) for line in f.readlines())
+
+def visit(n, a, visited, k, dp):
+    if visited + 1 == 1 << n:
+        ans = a[k][0]
+        return ans if ans else 1_000_000
+    if dp[visited][k] is not None:
+        return dp[visited][k]
+    bit = 1
+    ans = 1_000_000
+    for i in range(n):
+        if a[k][i] and not (visited & bit):
+            ans = min(ans, visit(n, a, visited | bit, i, dp) + a[k][i])
+        bit <<= 1
+    dp[visited][k] = ans
+    return ans
+
+dp = [[None] * n for _ in range(1 << n)]
+print(visit(n, a, 1, 0, dp))
